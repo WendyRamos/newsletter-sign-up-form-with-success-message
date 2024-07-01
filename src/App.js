9a074b5a845c5f8ba609303c338ce54e72d563a4
WendyRamos <wendymaribelr@gmail.com>
2024-06-30 21:24:24 -0500

    Se quita /

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/newsletter-sign-up-form-with-success-message/">
+    <Router basename="/newsletter-sign-up-form-with-success-message">
     <div className="App">
       <Routes>
         <Route path="/" element={<SignUp />} />
